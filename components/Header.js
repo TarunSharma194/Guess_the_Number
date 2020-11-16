2090da02a115b26fccb1725e20506feeaf84dae8
TarunSharma194 <tarunsharma23793@gmail.com>
2020-11-17 01:00:45 +0530

    use of Platform.select

--- components/Header.js
+++ components/Header.js
@@ -5,23 +5,29 @@ import Colors from '../constants/Colors';
 
 const Header = (props) => {
 	return (
-		<View style={styles.header}>
+		<View style={{...styles.headerBase, ...Platform.select({ios: styles.headerIOS, android: styles.headerAndroid})}}>
 			<Text style={styles.headerTitle}>{props.title}</Text>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	header: {
+	headerBase: {
 		width: '100%',
 		height: 90,
 		padding: 30,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
 		marginBottom: 20,
-		borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-		borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+		
+	},
+	headerIOS: {
+		backgroundColor: 'white',
+		borderBottomColor: '#ccc',
+		borderBottomWidth: 1
+	},
+	headerAndroid :{
+		backgroundColor: Colors.primary,
 	},
 	headerTitle: {
 		color: Platform.OS === 'android' ? 'white' : Colors.primary,
